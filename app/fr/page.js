6aa38d850e78bed20c59dd65110e16c5583b75ff
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-07-21 19:48:19 +0200

    update 1.2

--- app/fr/page.js
+++ app/fr/page.js
@@ -11,7 +11,7 @@ import { fadeIn } from "@/variants";
 
 export default function Home() {
   return (
-    <div className="bg-primary/60 h-full">
+    <div className="bg-primary/60 h-full overflow-y-auto">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1
